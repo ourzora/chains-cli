7c15bff77ac4cc0c0fcd640e6dc12d2d4e242985
Dan Oved <stangogh@gmail.com>
2024-01-29 13:25:03 -0800

    converted to typescript

--- index.ts
+++ index.ts
@@ -1,26 +1,33 @@
 #!/usr/bin/env node
-const { exec } = require('node:child_process');
-const yargs = require('yargs/yargs')
-const { hideBin } = require('yargs/helpers')
-const argv = yargs(hideBin(process.argv)).argv
-const wagmiChains = require('viem/chains')
-const {join} = require('path')
-const {homedir} = require('os');
-const fs = require('fs')
+import { exec } from 'node:child_process'
+import yargs from 'yargs'
+import { hideBin } from 'yargs/helpers'
+import wagmiChains, { Chain } from 'viem/chains'
+import {join} from 'path'
+import {homedir} from 'os'
+import fs from 'fs'
 
-const kebabize = (str) => 
+const kebabize = (str: string) => 
   str.replace(/[A-Z]+(?![a-z])|[A-Z]/g, ($, ofs) => (ofs ? "-" : "") + $.toLowerCase())
 
-const wagmiKnownChains = {} 
+type WagmiKnownChain = {
+    id: number,
+    rpcUrl: string | undefined,
+    blockExplorer: string | undefined,
+    etherscanUrl: string | undefined,
+    etherscanApiKey?: string,
+    verifierUrl?: string
+}
+const wagmiKnownChains: Record<string, WagmiKnownChain> = {} 
 
 const configDirectoryPrefix = '.chains'
 const configDirectory = join(homedir(), configDirectoryPrefix);
 
-function readConfigFile(path) {
+function readConfigFile(path: string) {
   const fullPath = join(configDirectory, path);
   if (fs.existsSync(fullPath)) {
     try {
-      return JSON.parse(fs.readFileSync(fullPath));
+      return JSON.parse(fs.readFileSync(fullPath, 'utf-8'));
     } catch (e) {
       console.error(e);
       return {};
@@ -32,14 +39,14 @@ function readConfigFile(path) {
 const globalConfig = readConfigFile('config.json');
 
 
-function updateChainConfig(config) {
+function updateChainConfig(config: Chain): WagmiKnownChain {
   let rpcUrl = config.rpcUrls.default?.http[0];
 
   if (globalConfig.alchemyApiKey && config.rpcUrls.alchemy) {
     rpcUrl = `${config.rpcUrls.alchemy.http[0]}/${globalConfig.alchemyApiKey}`;
   }
 
-  if (rpcUrl.includes('$alchemyApiKey')) {
+  if (rpcUrl?.includes('$alchemyApiKey')) {
     rpcUrl = rpcUrl.replace('$alchemyApiKey', globalConfig.alchemyApiKey);
   }
 
@@ -52,10 +59,10 @@ function updateChainConfig(config) {
 }
 
 for (const chain of Object.keys(wagmiChains)) {
-  wagmiKnownChains[kebabize(chain)] = updateChainConfig(wagmiChains[chain]);
+  wagmiKnownChains[kebabize(chain)] = updateChainConfig(wagmiChains[chain as keyof typeof wagmiChains]);
 }
 
-function forge(config, args) {
+function forge(config: WagmiKnownChain, args: { verify?: boolean, deploy?: boolean }) {
   if (args.verify) {
     const command = [`--chain ${config.id}`];
     if (config.etherscanApiKey) {
@@ -83,16 +90,16 @@ function forge(config, args) {
   console.log(command.join(' '));
 }
 
-function explorer(config) {
+function explorer(config: WagmiKnownChain) {
   exec(`open ${config.etherscanUrl || config.blockExplorer}`);
 }
 
-function etherscan(config) {
+function etherscan(config: WagmiKnownChain) {
   console.log(`${config.etherscanApiKey}`); 
 }
 
 
-function rpc(config) {
+function rpc(config: WagmiKnownChain) {
   console.log(config.rpcUrl);
 }
 
@@ -101,23 +108,28 @@ function updateChainsConfigRepo() {
   const command = 'git pull origin main';
   console.log(`running: ${command}`);
   const proc = exec(command, {cwd: configDirectory});
-  proc.stdout.pipe(process.stdout);
-  proc.stderr.pipe(process.stderr);
+  proc.stdout?.pipe(process.stdout);
+  proc.stderr?.pipe(process.stderr);
 }
 
-function getConfig(fn) {
-  return ({chain, ...rest}) => {
+function getConfig(fn: (config: WagmiKnownChain, args: any) => void) {
+  return ({chain, ...rest}: { chain: string, [key: string]: any }) => {
     const configFileOverrides = readConfigFile(`${chain}.json`);
     fn(Object.assign({}, wagmiKnownChains[chain], configFileOverrides), rest);
   }
 }
 
 yargs(hideBin(process.argv))
+  
+  // @ts-ignore
   .command(['forge <chain>', '$0 <chain>'], 'print output for forge', (yargs) => {
     yargs.option('verify', {desc: 'Verify contract'}).option('deploy', {desc: 'Deploy contract'})
   }, getConfig(forge))
+  // @ts-ignore
   .command('etherscan <chain>', 'print etherscan api key', () => {}, getConfig(etherscan))
+  // @ts-ignore
   .command('rpc <chain>', 'print rpc url', () => {}, getConfig(rpc))
+  // @ts-ignore
   .command('explorer <chain>', 'open block explorer', () => {}, getConfig(explorer))
   .command('update', 'Updates repo with remote main', () => {}, updateChainsConfigRepo)
   .parse()
